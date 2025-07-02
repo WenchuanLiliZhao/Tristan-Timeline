ba8bfbc342c9d98c4fdb89a6dcf4f0b563f4910a
Wenchuan Zhao <111623150+WenchuanLiliZhao@users.noreply.github.com>
2025-07-02 15:14:18 +0800

    1

--- react/src/pages/index.ts
+++ react/src/pages/index.ts
@@ -1,6 +1,8 @@
+
 import Home from "./Home";
 import NotFound from "./NotFound";
 import { DebugPages } from "./debug-pages";
+import React from 'react';
 
 export type DemoPageType = {
   name: string;
